821ed94eac48a0d558902b63edf1b58e2b5b8811
mazeneko <mazeneko.dev@gmail.com>
2025-02-02 16:14:08 +0900

    現在時刻のSignalのProviderを作成

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideCurrentDateTime } from './util/current-date-time-provider.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideCurrentDateTime(500),
   ],
 };
---
+++ src/app/util/current-date-time-provider.service.ts
@@ -0,0 +1,28 @@
+import { InjectionToken, Provider, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { LocalDateTime } from '@js-joda/core';
+import { interval, map } from 'rxjs';
+
+/**
+ * 現在日時のInjectionToken
+ */
+export const CURRENT_DATE_TIME = new InjectionToken<Signal<LocalDateTime>>(
+  'current.date.time',
+);
+/**
+ * 現在日時のSignalを提供します。
+ * {@link CURRENT_DATE_TIME}でInjectionできます。
+ * @param intervalPeriod Signalの更新間隔。関数内で{@link interval}に使われます。デフォルトは1000ms。
+ */
+export function provideCurrentDateTime(
+  intervalPeriod: number = 1000,
+): Provider {
+  return {
+    provide: CURRENT_DATE_TIME,
+    useFactory: () =>
+      toSignal(
+        interval(intervalPeriod).pipe(map((_) => LocalDateTime.now())), //
+        { initialValue: LocalDateTime.now() },
+      ),
+  };
+}
