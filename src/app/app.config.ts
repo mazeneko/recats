074d297797fee9c315ecdaa1344ca406a699ae41
mazeneko <mazeneko.dev@gmail.com>
2025-10-08 22:28:49 +0900

    zodのメッセージをカスタマイズ

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import {
   ApplicationConfig,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { configZodMessage } from './l10n/zod-message';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideAppInitializer(() => configZodMessage()), // zodのメッセージを設定します。
   ],
 };
