ee69d3bd2f5c424aefcd491b1f76cd6d05a591ff
mazeneko <mazeneko.dev@gmail.com>
2025-10-08 22:27:25 +0900

    壊れているテストを修正

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recats');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to recats');
   });
 });
